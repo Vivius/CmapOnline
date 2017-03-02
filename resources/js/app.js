e0520b5a2406e1b083eceeaa796c2ae71fa1c691
Vincent PEILLEX <peillex.vincent@live.fr>
2017-03-02 13:11:06 +0100

    Il est maintenant possible de supprimer et d'ajouter directement des noeuds dans le graphe.

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,43 +6,42 @@ var intersect = svgIntersections.intersect;
 var shape = svgIntersections.shape;
 
 // Variables globales
-var w = $(window).width();
-var h = $(window).height();
+var width = $(document).width();
+var height = $(document).height();
 var linkDistance = 200;
 var nodeWidth = 120;
 var nodeHeight = 80;
-
-// Palette de couleurs
 var colors = d3.scale.category10();
+var nodes, links, linkLabels;
 
 // Données à représenter.
 var dataset = {
     nodes: [
-        {name: "C++"},
-        {name: "C"},
-        {name: "Javascript"},
-        {name: "C#"},
-        {name: "Java"},
-        {name: "Smalltalk"},
-        {name: "PHP"},
-        {name: "LISP"},
-        {name: "Ruby"},
-        {name: "Python"}
+        {id: 0, name: "C++"},
+        {id: 1, name: "C"},
+        {id: 2, name: "Javascript"},
+        {id: 3, name: "C#"},
+        {id: 4, name: "Java"},
+        {id: 5, name: "Smalltalk"},
+        {id: 6, name: "PHP"},
+        {id: 7, name: "LISP"},
+        {id: 8, name: "Ruby"},
+        {id: 9, name: "Python"}
     ],
     links: [
-        {source: 0, target: 1},
-        {source: 0, target: 2},
-        {source: 0, target: 3},
-        {source: 0, target: 4},
-        {source: 1, target: 5},
-        {source: 2, target: 5},
-        {source: 2, target: 5},
-        {source: 3, target: 4},
-        {source: 5, target: 8},
-        {source: 5, target: 9},
-        {source: 6, target: 7},
-        {source: 7, target: 8},
-        {source: 8, target: 9}
+        {id: 0, source: 0, target: 1},
+        {id: 1, source: 0, target: 2},
+        {id: 2, source: 0, target: 3},
+        {id: 3, source: 0, target: 4},
+        {id: 4, source: 1, target: 5},
+        {id: 5, source: 2, target: 5},
+        {id: 6, source: 2, target: 5},
+        {id: 7, source: 3, target: 4},
+        {id: 8, source: 5, target: 8},
+        {id: 9, source: 5, target: 9},
+        {id: 10, source: 6, target: 7},
+        {id: 11, source: 7, target: 8},
+        {id: 12, source: 8, target: 9}
     ]
 };
 
@@ -51,97 +50,8 @@ var svg = d3
     .select("body")
     .append("svg")
     .attr({
-        "width": w,
-        "height": h
-    });
-
-// Configuration de la force du graphe.
-var force = d3.layout.force()
-    .nodes(dataset.nodes)
-    .links(dataset.links)
-    .size([w, h])
-    .linkDistance(linkDistance)
-    .charge(-800)
-    .theta(0.1)
-    .gravity(0.05)
-    .start();
-
-// Ajout des noeuds (cartes conceptuelles).
-var nodes = svg.selectAll(".node")
-    .data(dataset.nodes)
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .call(force.drag);
-nodes
-    .append("rect")
-    .attr({
-        "width": nodeWidth,
-        "height": nodeHeight
-    })
-    .style("fill", function (d, i) {
-        return colors(i);
-    });
-nodes
-    .append("text")
-    .attr({
-        "x": nodeWidth / 2,
-        "y": nodeHeight / 2,
-        "class": "node-label",
-        "stroke": "#000000",
-        "text-anchor": "middle"
-    })
-    .text(function (d) {
-        return d.name;
-    });
-
-// Liens entre les neouds (arrêtes).
-var links = svg.selectAll(".link")
-    .data(dataset.links)
-    .enter()
-    .append('path')
-    .attr({
-        'd': function (d) {
-            return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y + 'Z'
-        },
-        'class': 'link',
-        'fill-opacity': 0.5,
-        'stroke-opacity': 0.5,
-        'fill': '#000000',
-        'stroke': '#000000',
-        'id': function (d, i) {
-            return 'link-' + i
-        }
-    })
-    .attr('marker-end', 'url(#fleche)')
-    .style("pointer-events", "none");
-
-// Création des labels posés sur les arrêtes.
-var linkLabels = svg
-    .selectAll(".link-label")
-    .data(dataset.links)
-    .enter()
-    .append('text')
-    .style("pointer-events", "none")
-    .attr({
-        'class': 'link-label',
-        'id': function (d, i) {
-            return 'link-label-' + i
-        },
-        'dx': linkDistance / 2,
-        'dy': -10,
-        'font-size': 13,
-        'fill': '#000000',
-        "text-anchor": "middle"
-    });
-linkLabels
-    .append('textPath')
-    .attr('xlink:href', function (d, i) {
-        return '#link-' + i
-    })
-    .style("pointer-events", "none")
-    .text(function (d, i) {
-        return 'label ' + i
+        "width": width,
+        "height": height
     });
 
 // Création d'un marker en forme de flèche (définition).
@@ -159,17 +69,161 @@ svg
         'xoverflow': 'visible'
     })
     .append('svg:path')
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-    .attr('fill', '#000000')
-    .attr('stroke', '#000000');
+    .attr({
+        'd': 'M 0,-5 L 10 ,0 L 0,5',
+        'fill': '#000000',
+        'stroke': '#000000'
+    });
+
+// Configuration de la force du graphe.
+var force = d3.layout.force()
+    .nodes(dataset.nodes)
+    .links(dataset.links)
+    .size([width, height])
+    .linkDistance(linkDistance)
+    .charge(-800)
+    .theta(0.1)
+    .gravity(0.05)
+    .start();
+// Force events.
+force.drag().on("dragstart", nodeDragStart); // Drag des cartes conceptuelles.
+force.on("tick", forceTick); // Evénement tick du force layout.
+
+update();
+
+function update() {
+    nodes = svg.selectAll(".node")
+        .data(dataset.nodes, function (d) {
+            return d.id;
+        })
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        })
+        .call(force.drag);
+    nodes
+        .append("rect")
+        .attr({
+            "width": nodeWidth,
+            "height": nodeHeight
+        })
+        .style("fill", function (d, i) {
+            return colors(i);
+        });
+    nodes
+        .append("text")
+        .attr({
+            "x": nodeWidth/2,
+            "y": nodeHeight/2,
+            "class": "node-label",
+            "stroke": "#000000",
+            "text-anchor": "middle"
+        })
+        .text(function (d) {
+            return d.name;
+        });
+
+    // Liens entre les neouds (arrêtes).
+    links = svg.selectAll(".link")
+        .data(dataset.links, function (d) {
+            return d.id;
+        })
+        .enter()
+        .append('path')
+        .attr({
+            'd': function (d) {
+                return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y + 'Z'
+            },
+            'class': 'link',
+            'fill-opacity': 0.5,
+            'stroke-opacity': 0.5,
+            'fill': '#000000',
+            'stroke': '#000000',
+            'id': function (d, i) {
+                return 'link-' + i
+            },
+            'marker-end': 'url(#fleche)'
+        });
+
+    // Création des labels posés sur les arrêtes.
+    linkLabels = svg.selectAll(".link-label")
+        .data(dataset.links, function (d) {
+            return d.id;
+        })
+        .enter()
+        .append('text')
+        .attr({
+            'class': 'link-label',
+            'id': function (d, i) {
+                return 'link-label-' + i
+            },
+            'dx': linkDistance / 2,
+            'dy': -10,
+            'font-size': 13,
+            'fill': '#000000',
+            "text-anchor": "middle"
+        });
+    linkLabels
+        .append('textPath')
+        .attr('xlink:href', function (d, i) {
+            return '#link-' + i
+        })
+        .text(function (d, i) {
+            return 'label ' + i
+        });
+
+    // Mise à jour des références avec les nouveaux noeuds ajoutés.
+    nodes = svg.selectAll('.node');
+    links = svg.selectAll(".link");
+    linkLabels = svg.selectAll(".link-label");
+
+    // Lorqu'on double clic sur une carte, on la libère.
+    nodes.on("dblclick", nodeDbClick);
+
+    // Rafraichissement du force layout.
+    force.start();
+}
+
+// Supprime le noeud passé en paramètre du graphe.
+function removeNode(node) {
+    var linksToDelete = [];
+
+    dataset.nodes.splice(dataset.nodes.indexOf(node), 1);
+    $.each(dataset.links, function (i, link) {
+        if(link.source.id == node.id || link.target.id == node.id)
+            linksToDelete.push(link);
+    });
+    $.each(linksToDelete, function (i, link) {
+        dataset.links.splice(dataset.links.indexOf(link), 1);
+    });
+    linkLabels
+        .data(dataset.links, function (d) {
+            return d.id;
+        })
+        .exit()
+        .remove();
+    links
+        .data(dataset.links, function (d) {
+            return d.id;
+        })
+        .exit()
+        .remove();
+    nodes
+        .data(dataset.nodes, function (d) {
+            return d.id;
+        })
+        .exit()
+        .remove();
+}
 
 /******************************************************************
  * EVENTS                                                         *
  ******************************************************************/
 
 // Event lancé lorsque les cartes bougent (drag, gravité, force...).
-force.on("tick", function () {
-
+function forceTick() {
     // Mise à jour du positionnement des cartes.
     nodes.attr("transform", function (d) {
         return "translate(" + d.x + "," + d.y + ")";
@@ -206,49 +260,37 @@ force.on("tick", function () {
     // Aligne le label des liens au centre.
     linkLabels.attr("dx", function () {
         var linkId = d3.select(this).select("textPath").attr("xlink:href");
-        return $(linkId)[0].getTotalLength() / 2;
+        var link = $(linkId)[0];
+        if(link)
+            return link.getTotalLength()/2;
+        else
+            return linkDistance/2;
     });
-});
+}
 
-// Lorqu'une carte est déplacée, on fixe sa position.
-force.drag().on("dragstart", function (d) {
+// Lorque l'on commence à bouger une carte, on la fixe.
+function nodeDragStart(d) {
     d3.select(this).classed("fixed", d.fixed = true);
-});
+}
 
-// Lorqu'on double clic sur une carte, on la libère.
-nodes.on("dblclick", function (d) {
+// Lorque l'on doule clic sur une carte, on la libère. Le force layout reprend le contrôle.
+function nodeDbClick(d) {
     d3.select(this).classed("fixed", d.fixed = false);
-});
+}
 
 /******************************************************************
  * TESTS                                                          *
  ******************************************************************/
 
-/*
 // Ajout à la volée.
 setTimeout(function () {
-    dataset.nodes.push({name: "Vincent"});
-    svg.selectAll("rect")
-        .data(dataset.nodes)
-        .enter()
-        .append("rect")
-        .attr({
-            "width": nodeWidth,
-            "height": nodeHeight
-        })
-        .style("fill", function (d, i) {
-            return colors(i);
-        })
-        .call(force.drag);
-    console.log("Ajout de Vincent");
-}, 2000);
+    console.log("Ajout de Linux");
+    dataset.nodes.push({id: 11, name: "Linux", x:100, y:100});
+    update();
+ }, 2000);
+
 // Suppression à la volée.
 setTimeout(function () {
-    dataset.nodes.splice(1, 1);
-    nodes
-        .data(dataset.nodes)
-        .exit()
-        .remove();
-    console.log("Suppression de Bob");
+    console.log("Suppression de C++");
+    removeNode(dataset.nodes[0]);
 }, 5000);
-*/
