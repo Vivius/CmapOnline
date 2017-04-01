b3c86c9195c639931fa6310ea61d5f67136d783e
Vivius <peillex.vincent@live.fr>
2017-04-01 17:24:32 +0200

    Ajout de la connexion à MongoDB avec une route d'exemple de création de noeud.

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var mongo = require("mongodb").MongoClient;
+var objectID = require("mongodb").ObjectID;
+var DB = "mongodb://localhost/cmap";
 
+// Test de onnection à MongoDB.
+mongo.connect(DB, function(error, db) {
+    if(error) throw error;
+    console.log("Connecté à la base de données 'cmap'");
+});
+
+// Configuration du framework express.
 var app = express();
 app.use("/css",express.static(__dirname + '/css'));
 app.use("/images",express.static(__dirname + '/images'));
@@ -19,6 +29,15 @@ app.get("/getGraph/:id", function (req, res) {
     res.json({ graph: { nodes: {}, links: {} } });
 });
 
+app.get("/node/create", function (req, res) {
+    mongo.connect(DB, function(error, db) {
+        db.collection("nodes").insert({ name:"New", x:0, y:0, fixed:true}, null, function (error, results) {
+            if (error) throw error;
+            res.json(results.ops[0]);
+        });
+    });
+});
+
 //------------------------------------------------------------
 // ERRORS
 //------------------------------------------------------------
@@ -34,7 +53,7 @@ app.use(function(req, res, next){
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     socket.on("new-node", function (node) {
         console.log("ADD NODE : " + node.id);
         io.emit("new-node-response", node);
@@ -42,7 +61,7 @@ io.on('connection', function(socket){
     socket.on("remove-node", function (node) {
         console.log("REMOVE NODE : " + node.id);
         io.emit("remove-node-response", node);
-    })
+    });
 });
 
 http.listen(8080);
