d3047e2fcc6fc04cc7a5bfb0cdbaeaad40b3fefe
Victor <victor.basset@yahoo.fr>
2017-05-09 15:01:45 +0200

    Home.html design terminé. Connection avec la base en cours de réalisation...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
 $(document).ready(function() {
     $("#new").click(function() {
-        console.log("tokokfzokf");
         var h = $("body").height() + 'px';
         $("#black_overlay").css({"height":h,"visibility":"visible"});
         $(".added").css('display','block');
+        $(".added").css('visibility','visible');
     });
 
     $(".close").click(function() {
